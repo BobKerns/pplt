8dea1ef6411cfd93172c814b609144506a598dd9
Bob Kerns <1154903+BobKerns@users.noreply.github.com>
2025-01-05 11:41:40 -0800

    Support scenario import  and some input validation.

--- src/pplt/loader.py
+++ src/pplt/loader.py
@@ -27,9 +27,13 @@ type. The modules that define these types define `valid_xxx` functions that can 
 to validate the values, and convert if necessary. For example,
 '''
 
+from collections.abc import Iterable
 from datetime import date
+from itertools import count
 from pathlib import Path
-from typing import NotRequired, Protocol, TypedDict, cast
+from typing import Literal, NotRequired, Protocol, TypedDict, cast
+import sys
+from inspect import signature
 
 import yaml
 
@@ -43,22 +47,30 @@ from pplt.period import Period, PeriodUnit, Periodic, valid_period_unit
 from pplt.timeline_series import timeline, Timeline
 from pplt.transaction import transfer
 
-class LoaderEntry(TypedDict):
+class LoaderEntry[T: str](TypedDict):
     '''
     A loader entry. That is, a YAML document in a loader file.
 
     Note that YAML files can contain multiple documents.
     '''
-    uid: NotRequired[str]
+    id: NotRequired[str|int]
     '''
     A unique identifier for the entry. This will be used to identify entries
     that may be overridden by scenarios that include the loader file.
     '''
-    type: str
+    type: T
     '''
     The type of entry. This will determine how the entry is loaded.
-    It must be the name of a loader function in the `LOADERS` dictionary.
+    It must be the name of a loader function in the `LOADERS` dictionary,
+    or the special type `import`.
     '''
+
+class LoaderImportEntry(LoaderEntry[Literal['import']]):
+    '''
+    A loader entry that imports another scenario.
+    '''
+    type: Literal['import']
+    file: str
 
 type Loadable = Account|UpdateHandler
 '''
@@ -66,23 +78,23 @@ A type alias for the types that can be loaded by a loader.
 '''
 
 
-class Loader[E: LoaderEntry, T: Loadable](Protocol):
+class Loader[T: str, L: Loadable](Protocol):
     '''
     A loader function for a specific type of entry and loadable object in
     the YAML files.
     '''
-    def __call__(self, entry: E) -> T:
+    def __call__(self, entry: LoaderEntry[T]) -> L:
         ...
 
-LOADERS: dict[str, Loader[LoaderEntry, Loadable]] = {}
+LOADERS: dict[str, Loader[str, Loadable]] = {}
 
 
-def add_loader[E: LoaderEntry](type_: str,
-                               loader: Loader[E, Loadable]):
+def add_loader[T: str, L: Loadable](type_name: T, loader: Loader[T, L]):
     '''
     Add a loader to the `LOADERS` dictionary.
     '''
-    LOADERS[type_] = cast(Loader[LoaderEntry, Loadable], loader)
+    # Complains if we cast as unnecessary (as is proper) but complains if we don't cast.
+    LOADERS[type_name] = loader # type: ignore
 
 
 class LoaderValue_(TypedDict):
@@ -138,14 +150,15 @@ def load_rate(entry: LoaderRate) -> float:
     period = valid_period_unit(entry.get('period', 'year'))
     return apr(percent / 100.0, period)
 
-class LoaderAccount(LoaderEntry, LoaderValue_):
+class LoaderAccount(LoaderEntry[Literal['account']], LoaderValue_):
     name: str
 
 def load_account(entry: LoaderAccount) -> Account:
     value = load_value(entry)
     return Account(entry['name'], value)
 
-add_loader('account', load_account)
+# Fails to recognize the LoaderValue_ in the inheritance
+add_loader('account', load_account) # type: ignore
 
 class LoaderStart(TypedDict):
     start: NotRequired[Month]
@@ -154,7 +167,7 @@ def load_start(entry: LoaderStart) -> date:
     start = entry.get('start', None)
     return next_month() if start is None else valid_month(start)
 
-class LoaderInterest(LoaderEntry):
+class LoaderInterest(LoaderEntry[Literal['interest']]):
     '''
     A loader entry for interest. Interest is give as an annual percentage rate (APR)
     unless a period is specified.
@@ -169,7 +182,7 @@ def load_interest(entry: LoaderInterest) -> UpdateHandler:
     start = load_start(entry)
     return interest(entry['account'], start, rate=rate*100)
 
-add_loader('interest', load_interest)
+add_loader('interest', load_interest) # type: ignore
 
 LoaderFrom = TypedDict('LoaderFrom', {'from': str})
 '''
@@ -177,14 +190,14 @@ A type alias for entries that have a `from` field,
 since this can't be a field name in a TypedDict.
 '''
 
-class TransferLoader(LoaderEntry, LoaderFrom, LoaderStart):
+class LoaderTransfer(LoaderEntry[Literal['transfer']], LoaderFrom, LoaderStart):
     to: str
     amount: LoaderValue
     period: LoaderPeriod
     from_min: NotRequired[LoaderValue]
     to_max: NotRequired[LoaderValue]
 
-def load_transfer(entry: TransferLoader) -> UpdateHandler:
+def load_transfer(entry: LoaderTransfer) -> UpdateHandler:
     '''
     Load a transfer from a loader entry.
     '''
@@ -199,25 +212,132 @@ def load_transfer(entry: TransferLoader) -> UpdateHandler:
                     from_min=from_min,
                     to_max=to_max)
 
-add_loader('transfer', load_transfer)
+add_loader('transfer', load_transfer) # type: ignore
+
+class LoaderImport(LoaderEntry[Literal['import']]):
+    file: str
+
+def genid():
+    '''
+    Generate a unique ID for an entry.
+    '''
+    _counter = count()
+    return f'__{next(_counter)}'
+
+def entry_id(entry: LoaderEntry[str]) -> str|int:
+    '''
+    Get the ID for an entry.
+    '''
+    return entry.get('id', None) or entry.get('name', None) or genid()
+
+def valid_loader_entry(entry: LoaderEntry[str]|None) -> LoaderEntry[str]|None:
+    '''
+    Validate a loader entry. Validation is not complete: It does not check subfields, nor that
+    the fields are the right type. but its should catch most typos.
+    '''
+    if entry is None:
+        return None
+    id_ = entry_id(entry)
+    entry['id'] = id_
+    type_ = entry['type']
+    match type_:
+        # Handle 'import' specially, as it is not a real loader type,
+        # but a directive to import another file.
+        case 'import':
+            if 'file' not in entry:
+                raise ValueError('Import entry must have a file field')
+            return entry
+        # Verify that it's a valid loader type
+        case _:
+            if not type_ in LOADERS:
+                print(f'Unknown loader type: {type_}', file=sys.stderr)
+                return None
+    loader = LOADERS[type_]
+    # Get the type of the first parameter of the loader function
+    sig = signature(loader)
+    p0_name = list(sig.parameters)[0]
+    p0 = cast(type, sig.parameters[p0_name].annotation)
+    # Check that the required keys are present
+    for k in p0.__required_keys__: # type: ignore
+        if k not in entry:
+            print(f'Missing required key {k} for {type_}', file=sys.stderr)
+            return None
+    # Check that there are no unknown keys
+    for k in entry.keys():
+        if k not in p0.__required_keys__ and k not in p0.__optional_keys__: # type: ignore
+            print(f'Unknown key {k} for {type_}', file=sys.stderr)
+            return None
+    return entry
+
+def load_scenario_yaml(path: Path|str) -> dict[str|int, LoaderEntry[str]]:
+    '''
+    Load the `LoaderEntry` objects from a YAML file, to be merged with
+    imports of other scenarios.
+
+
+    '''
+    path = Path(path)
+    with path.open() as f:
+        by_id: dict[str|int, LoaderEntry[str]] = {}
+        # Load the entries from the YAML file
+        entries = cast(Iterable[LoaderEntry[str]], yaml.safe_load_all(f))
+
+        entries = sorted((e for e in entries if e),
+                         key=lambda entry: entry_id(entry))
+        for entry in entries:
+            match entry.get('type', None):
+                case 'import':
+                    entry_ = cast(LoaderImport, entry)
+                    file = entry_['file']
+                    imported = load_scenario_yaml(path.parent / file)
+                    for id_, entry_ in imported.items():
+                        if id_ in by_id:
+                            # An import overriding an entry from a prior import
+                            entry_.update(by_id[id_])
+                        by_id[id_] = entry_
+                case _:
+                    pass
+        for entry in entries:
+            match entry.get('type', None):
+                case 'import':
+                    pass
+                case _:
+                    id_ = entry_id(entry)
+                    if id_ in by_id:
+                        # Overriding an imported entry
+                        by_id[id_].update(entry)
+                    else:
+                        by_id[id_] = entry
+        return by_id
 
 def load_scenario(path: Path|str) -> Timeline:
     '''
     Load a scenario from a YAML file.
     '''
-    path = Path(path)
-    with path.open() as f:
-        entries = cast(list[LoaderEntry], yaml.safe_load_all(f))
-        def load_item(entry: LoaderEntry):
-            type_ = entry['type']
-            loader = LOADERS.get(type_)
-            if loader is None:
-                raise ValueError(f'Unknown loader type: {type_}')
-            return loader(entry)
-        items = [load_item(entry) for entry in entries]
-        accounts = {item.name: item for item in items if isinstance(item, Account)}
-        schedule = Schedule([item for item in items if isinstance(item, UpdateHandler)])
-        return timeline(schedule, **accounts)
+    by_id = load_scenario_yaml(path)
+    def load_item(entry: LoaderEntry[str]):
+        type_ = entry['type']
+        loader = LOADERS.get(type_)
+        if loader is None:
+            raise ValueError(f'Unknown loader type: {type_}')
+        return loader(entry)
+    items = [
+        load_item(entry)
+        for entry in by_id.values()
+        if entry['type'] != 'import'
+    ]
+    accounts = {
+        item.name: item
+        for item in items
+        if isinstance(item, Account)
+    }
+    handlers = [
+        item
+        for item in items
+        if isinstance(item, UpdateHandler)
+    ]
+    schedule = Schedule(handlers)
+    return timeline(schedule, **accounts)
 
 
 if __name__ == '__main__':
